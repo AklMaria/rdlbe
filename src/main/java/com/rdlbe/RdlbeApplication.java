fcf007454382cacf9d655f8bdbe90ffcf52ba361
mariaakl <maria2000akl@gmail.com>
2025-07-18 16:48:23 +0200

    first commit

---
+++ src/main/java/com/rdlbe/RdlbeApplication.java
@@ -0,0 +1,13 @@
+package com.rdlbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdlbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdlbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rdlbe/RdlbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdlbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdlbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
