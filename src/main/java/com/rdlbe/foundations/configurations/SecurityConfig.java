410dd9d5ba69d0490232540746151d817be8f424
AklMaria <maria2000akl@gmail.com>
2025-09-25 09:52:53 +0200

    update security config file

--- src/main/java/com/rdlbe/foundations/configurations/SecurityConfig.java
+++ src/main/java/com/rdlbe/foundations/configurations/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 CorsConfigurationSource source = request -> {
                     CorsConfiguration config = new CorsConfiguration();
 
-                    config.setAllowedOrigins(List.of("http://localhost:4200"));
+                    config.setAllowedOrigins(List.of("http://localhost:4200", "https://frontend-426407479258.europe-west9.run.app", "https://redditodiliberta.it"));
 
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
